cbd7c77f10139ac62f2475f00545458c4ee0f2ce
frojer4444 <145400008+frojer4444@users.noreply.github.com>
2023-10-03 19:08:56 -0500

    #25 refactor s3 bucket into the new terrahouse_aws module (#25)

--- variables.tf
+++ variables.tf
@@ -1,19 +1,6 @@
 variable "user_uuid" {
-  type        = string
-  description = "User UUID in a specific format"
-  
-  validation {
-    condition     = can(regex("^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})$", var.user_uuid))
-    error_message = "User UUID must be in the format of 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' where x is a hexadecimal digit."
-  }
+    type = string
 }
-
 variable "bucket_name" {
-  type        = string
-  description = "Name of the AWS S3 bucket"
-
-  validation {
-    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "Bucket name must be between 3 and 63 characters, and can only contain lowercase letters, numbers, hyphens, and periods. It must start and end with a lowercase letter or number."
-  }
+    type = string
 }
